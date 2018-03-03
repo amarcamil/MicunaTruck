bc284443c1fbc88ef37a1f3e141e8706058f5d3c
Miguel Ángel Poma Hinostroza <pomahinostroza.miguel@gmail.com>
2018-03-03 15:49:23 -0500

    Se agrega models.

---
+++ src/pe/edu/utp/micunatruck/models/Event.java
@@ -0,0 +1,159 @@
+package pe.edu.utp.micunatruck.models;
+
+import java.text.Format;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Event {
+
+    private int id;
+    private User user;
+    private EventStatus eventStatus;
+    private String name;
+    private String description;
+    private String image;
+    private int flagActive;
+    private Date date;
+    private Date deleteAt;
+    private Date updatedAt;
+    private Date createdAt;
+    private boolean isSendRequest = false;
+
+    public Event(){
+
+    }
+
+    public Event(int id, User user, EventStatus eventStatus, String name, String description, String image, int flagActive, String date){
+        this.setId(id);
+        this.setUser(user);
+        this.setEventStatus(eventStatus);
+        this.setName(name);
+        this.setDescription(description);
+        this.setImage(image);
+        this.setDate(date);
+        this.setFlagActive(flagActive);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Event setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Event setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Event setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Event setImage(String image) {
+        this.image = image;
+        return this;
+    }
+
+    public int getFlagActive() {
+        return flagActive;
+    }
+
+    public Event setFlagActive(int flagActive) {
+        this.flagActive = flagActive;
+        return this;
+    }
+
+    public Date getDeleteAt() {
+        return deleteAt;
+    }
+
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Event setUser(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public EventStatus getEventStatus() {
+        return eventStatus;
+    }
+
+    public Event setEventStatus(EventStatus eventStatus) {
+        this.eventStatus = eventStatus;
+        return this;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Event setDate(String date) {
+        Date dateParse = null;
+        boolean parseableEnter = false;
+        try {
+            if(date != null && date != ""){
+                parseableEnter = true;
+                dateParse = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+            }
+        } catch (ParseException e) {
+            //TODO: quitar este try catch
+            if(parseableEnter){
+                try {
+                    dateParse = new SimpleDateFormat("yyyy-MM-dd").parse(date);
+                } catch (ParseException e1) {
+                    dateParse = null;
+                }
+            }
+            e.printStackTrace();
+        }
+        this.date = dateParse;
+        return this;
+    }
+
+    public String getDateParse(){
+        Format formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String date = "";
+        try{
+            date = formatter.format(this.getDate());
+        }catch (Exception e) {
+            date = "-";
+        }
+        return date;
+    }
+
+    public boolean getIsSendRequest() {
+        return isSendRequest;
+    }
+
+    public void setIsSendRequest(boolean sendRequest) {
+        this.isSendRequest = sendRequest;
+    }
+}
---
+++ src/pe/edu/utp/micunatruck/models/Finance.java
@@ -0,0 +1,130 @@
+package pe.edu.utp.micunatruck.models;
+
+import java.util.Date;
+
+public class Finance {
+    private int id;
+    private int originId;
+    private int originTypeId;
+    private double amount;
+    private Date startDate;
+    private Date endDate;
+    private boolean flagActive;
+    private Date updateAt;
+    private Date deletedAt;
+    private Date createdAt;
+
+    public Finance() {
+
+    }
+
+    public Finance(int id, int originId, int originTypeId, double amount, Date startDate, Date endDate, boolean flagActive) {
+
+    }
+
+    public Finance(int id, int originId, int originTypeId, double amount, Date startDate, Date endDate,
+                   boolean flagActive, Date updateAt, Date deletedAt, Date createdAt) {
+        this.id = id;
+        this.originId = originId;
+        this.originTypeId = originTypeId;
+        this.amount = amount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.flagActive = flagActive;
+        this.updateAt = updateAt;
+        this.deletedAt = deletedAt;
+        this.createdAt = createdAt;
+    }
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public Finance setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public int getOriginId() {
+        return originId;
+    }
+
+    public Finance setOriginId(int originId) {
+        this.originId = originId;
+        return this;
+    }
+
+    public int getOriginTypeId() {
+        return originTypeId;
+    }
+
+    public Finance setOriginTypeId(int originTypeId) {
+        this.originTypeId = originTypeId;
+        return this;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Finance setAmount(double amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Finance setStartDate(Date startDate) {
+        this.startDate = startDate;
+        return this;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public Finance setEndDate(Date endDate) {
+        this.endDate = endDate;
+        return this;
+    }
+
+    public boolean getFlagActive() {
+        return flagActive;
+    }
+
+    public Finance setFlagActive(boolean flagActive) {
+        this.flagActive = flagActive;
+        return this;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public Finance setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+        return this;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public Finance setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+        return this;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Finance setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+}
